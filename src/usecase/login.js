bfedab7c6d5d30754df446867981efc3cadec670
Shirleen <shirleen.adriana@gmail.com>
2021-10-13 22:26:15 +0700

    generate jwt token for successful login

--- src/usecase/login.js
+++ src/usecase/login.js
@@ -1,20 +1,25 @@
 const accountRepo = require("../repository/accounts");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../lib/error/error");
+const jwt = require("jsonwebtoken");
 
 async function login(username, password) {
   let account = await accountRepo.getAccount(username);
 
   return new Promise((resolve, reject) => {
-    if (!account) {
+    if (!account || account.password != password) {
       reject(new CustomError(StatusCodes.UNAUTHORIZED));
     }
 
-    if (account.password != password) {
-      reject(new CustomError(StatusCodes.UNAUTHORIZED));
-    }
+    let token = jwt.sign(
+      { id: account.id, username: account.username },
+      "secret!?",
+      {
+        expiresIn: "72h",
+      }
+    );
 
-    resolve({ id: account.id, username: account.username });
+    resolve({ id: account.id, username: account.username, token: token });
   });
 }
 
