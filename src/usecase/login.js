3151cb689dad7f39daeb9155cc36967dceecc8d6
Shirleen <shirleen.adriana@gmail.com>
2021-10-13 22:42:46 +0700

    implement bcrypt for password hash

--- src/usecase/login.js
+++ src/usecase/login.js
@@ -1,25 +1,24 @@
 const accountRepo = require("../repository/accounts");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../lib/error/error");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 async function login(username, password) {
   let account = await accountRepo.getAccount(username);
 
-  return new Promise((resolve, reject) => {
-    if (!account || account.password != password) {
-      reject(new CustomError(StatusCodes.UNAUTHORIZED));
+  return new Promise(async (resolve, reject) => {
+    if (account && (await bcrypt.compare(password, account.password))) {
+      let token = jwt.sign(
+        { id: account.id, username: account.username },
+        "secret!?",
+        {
+          expiresIn: "72h",
+        }
+      );
+      resolve({ id: account.id, username: account.username, token: token });
     }
-
-    let token = jwt.sign(
-      { id: account.id, username: account.username },
-      "secret!?",
-      {
-        expiresIn: "72h",
-      }
-    );
-
-    resolve({ id: account.id, username: account.username, token: token });
+    reject(new CustomError(StatusCodes.UNAUTHORIZED));
   });
 }
 
