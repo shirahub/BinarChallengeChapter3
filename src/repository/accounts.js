2c157631e9cb6927d051ac76dbeb979ff6be6c31
Shirleen <shirleen.adriana@gmail.com>
2021-10-13 23:30:28 +0700

    add error if read file failed

--- src/repository/accounts.js
+++ src/repository/accounts.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../lib/error/error");
 
 const getAccount = (username) => {
   return new Promise(function (resolve, reject) {
-    fs.readFile("src/repository/accounts.json", (err, data) => {
+    fs.readFile("src/repository/acconts.json", (err, data) => {
       if (err) {
-        reject(err);
+        reject(new CustomError(StatusCodes.INTERNAL_SERVER_ERROR));
       } else {
         let accounts = JSON.parse(data);
         temp = accounts.find((a) => a.username == username);
