a0f16a3b4d34c66127917099faecfb42fa774bb6
Shirleen <shirleen.adriana@gmail.com>
2021-10-13 23:03:34 +0700

    add request validator

--- src/controller/accounts.js
+++ src/controller/accounts.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const accountHandler = require("../usecase/login");
 const apiStatus = require("../lib/api_status");
+const { body, validationResult } = require("express-validator");
+const CustomError = require("../lib/error/error");
+const { StatusCodes } = require("http-status-codes");
+
 var api = express.Router();
 
-api.post("/login", function (req, res, next) {
-  accountHandler
-    .login(req)
-    .then((result) => {
-      apiStatus(res, result, 200);
-    })
-    .catch((err) => {
-      apiStatus(res, err);
-    });
-});
+api.post(
+  "/login",
+  body("username").isString().notEmpty(),
+  body("password").isString().notEmpty(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log("123");
+      return apiStatus(res, new CustomError(StatusCodes.BAD_REQUEST));
+    }
+    accountHandler
+      .login(req)
+      .then((result) => {
+        apiStatus(res, result, 200);
+      })
+      .catch((err) => {
+        apiStatus(res, err);
+      });
+  }
+);
 
 module.exports = api;
